9b655bc08451d19dfff32024902b7446c821e5ef
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-26 22:54:30 +0545

    getting index of element in an array

--- 30-Days-of-React/main.js
+++ 30-Days-of-React/main.js
@@ -1,2 +1,6 @@
-const numbers = [1,2,3,4,6,6];
-console.log(numbers.length);
+const numbers = [1,2,3,4,5];
+console.log(numbers.indexOf(1));
+console.log(numbers.indexOf(2));
+console.log(numbers.indexOf(3));
+console.log(numbers.indexOf(4));
+console.log(numbers.indexOf(5));
