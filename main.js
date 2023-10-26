9b8e168f712c4075f77cf92773e6824f3411094d
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-26 20:41:56 +0545

    arrays holding different types of data

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
-const numbers = [1,2,3,4,5,6,7];
-const fruits  = ['banana','cherries','peaches','mango'];
-const vegetables  = ['potato','carrot','cabbage','tomato'];
-const animalProducts = ['milk','meat','butter','yoghurt'];
-const webTechs = ['HTML','CSS','JS','React','Redux','Node'];
-const countries = ['Finland','Nepal','India','China'];
+const arr = [
+    'Zen Sama',
+    300,
+    true,
+    {country:'Nepal',city:'Kathmandu'},
+    {skills: ['HTML','CSS','JS','Python','Django','React']}
+];
 
-console.log('Numbers:',numbers);
-console.log("Number of numbers:",numbers.length);
-
-console.log('Fruits:', fruits);
-console.log('Number of fruits:', fruits.length);
-
-console.log('Vegetables:', vegetables);
-console.log('Number of vegetables:', vegetables.length);
-
-console.log('Animal products:', animalProducts);
-console.log('Number of animal products:', animalProducts.length);
-
-console.log('Web technologies:', webTechs);
-console.log('Number of web technologies:', webTechs.length);
-
-console.log('Countries:', countries);
-console.log('Number of countries:', countries.length);
+console.log(arr);
