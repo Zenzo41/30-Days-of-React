0862510f4c409a8963440d8a701dd27744764386
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-26 20:38:17 +0545

    printing the values of arrays and its length

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-console.log('welcome from the external link');
+const numbers = [1,2,3,4,5,6,7];
+const fruits  = ['banana','cherries','peaches','mango'];
+const vegetables  = ['potato','carrot','cabbage','tomato'];
+const animalProducts = ['milk','meat','butter','yoghurt'];
+const webTechs = ['HTML','CSS','JS','React','Redux','Node'];
+const countries = ['Finland','Nepal','India','China'];
 
-const arr = Array()
-console.log(arr);
+console.log('Numbers:',numbers);
+console.log("Number of numbers:",numbers.length);
 
-const arr1 = new Array();
-console.log(arr1);
+console.log('Fruits:', fruits);
+console.log('Number of fruits:', fruits.length);
 
-const arr2 = []; // Preferred mostly
-console.log(arr2);
+console.log('Vegetables:', vegetables);
+console.log('Number of vegetables:', vegetables.length);
+
+console.log('Animal products:', animalProducts);
+console.log('Number of animal products:', animalProducts.length);
+
+console.log('Web technologies:', webTechs);
+console.log('Number of web technologies:', webTechs.length);
+
+console.log('Countries:', countries);
+console.log('Number of countries:', countries.length);
