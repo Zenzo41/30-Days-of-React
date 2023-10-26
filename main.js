82bfa1ce9720b65ce022bb25e485af961126d9b7
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-26 20:31:46 +0545

    creating empty arrays

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ console.log('welcome from the external link');
 
 const arr = Array()
 console.log(arr);
+
+const arr1 = new Array();
+console.log(arr1);
+
+const arr2 = []; // Preferred mostly
+console.log(arr2);
