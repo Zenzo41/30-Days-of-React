dd17bf455b39c4d3bc697ae5817ae7f9ba321ea7
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-27 11:25:12 +0545

    fruits' index checked

--- 30-Days-of-React/main.js
+++ 30-Days-of-React/main.js
@@ -1,6 +1,8 @@
-const numbers = [1,2,3,4,5];
-console.log(numbers.indexOf(1));
-console.log(numbers.indexOf(2));
-console.log(numbers.indexOf(3));
-console.log(numbers.indexOf(4));
-console.log(numbers.indexOf(5));
+const fruits = ['banana', 'orange', 'mango', 'lemon']
+let index = fruits.indexOf('banana') // 0
+
+if (index != -1) {
+  console.log('This fruit exists in the array')
+} else {
+  console.log('This fruit does not exist in the array')
+}
