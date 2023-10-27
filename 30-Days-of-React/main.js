e055319b228d82e122462874414224eccca15516
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-27 11:49:32 +0545

    check if array includes an element

--- 30-Days-of-React/main.js
+++ 30-Days-of-React/main.js
@@ -1,5 +1,4 @@
-const numbers = [1,2,3,4,5,3,2,1,4,5];
+const numbers =[1,2,3,4,5];
 
-console.log(numbers.lastIndexOf(2));
-console.log(numbers.lastIndexOf(1));
-console.log(numbers.lastIndexOf(5));
+console.log(numbers.includes(3));
+console.log(numbers.includes(9));
