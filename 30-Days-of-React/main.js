f1d5932bf4902bef31904cafdb49be8c8d056606
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-27 11:34:42 +0545

    check lastIndexOf array element

--- 30-Days-of-React/main.js
+++ 30-Days-of-React/main.js
@@ -1,8 +1,5 @@
-const fruits = ['banana', 'orange', 'mango', 'lemon']
-let index = fruits.indexOf('banana') // 0
+const numbers = [1,2,3,4,5,3,2,1,4,5];
 
-if (index != -1) {
-  console.log('This fruit exists in the array')
-} else {
-  console.log('This fruit does not exist in the array')
-}
+console.log(numbers.lastIndexOf(2));
+console.log(numbers.lastIndexOf(1));
+console.log(numbers.lastIndexOf(5));
